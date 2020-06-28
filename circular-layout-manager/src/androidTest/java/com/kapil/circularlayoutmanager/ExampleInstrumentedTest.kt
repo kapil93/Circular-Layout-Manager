2a336f4cb7c4ea1c5398f18eacd6989fa55b2ed4
Kapilesh Tharaknath <kapilesh.tharaknath@intive.com>
2020-06-29 00:32:13 +0200

    Fix tests

--- circular-layout-manager/src/androidTest/java/com/kapil/circularlayoutmanager/ExampleInstrumentedTest.kt
+++ circular-layout-manager/src/androidTest/java/com/kapil/circularlayoutmanager/ExampleInstrumentedTest.kt
@@ -14,10 +14,9 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    @Throws(Exception::class)
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().context
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         Assert.assertEquals("com.kapil.circularlayoutmanager.test", appContext.packageName)
     }
 }
--- circular-layout-manager/src/test/java/com/kapil/circularlayoutmanager/ExampleUnitTest.kt
+++ circular-layout-manager/src/test/java/com/kapil/circularlayoutmanager/ExampleUnitTest.kt
@@ -10,7 +10,6 @@ import org.junit.Test
  */
 class ExampleUnitTest {
     @Test
-    @Throws(Exception::class)
     fun addition_isCorrect() {
         Assert.assertEquals(4, 2 + 2.toLong())
     }
