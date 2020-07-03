3f57392f4647c9929e9c684a4f4a480de65d5f35
Kapilesh Tharaknath <kapilesh.tharaknath@intive.com>
2020-07-03 16:03:54 +0200

    Convert file to kotlin

---
+++ app/src/test/java/kapil/circularlayoutmanager/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+package kapil.circularlayoutmanager
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2.toLong())
+    }
+}
